a363acc47b6df1d9c90740e2aff191a74c48c037
Nikolaos Batzakakis <nikolaos.batzakakis@agentes.de>
2023-01-04 21:18:35 +0100

    Initial commit

---
+++ src/test/java/de/nb/azn/AznApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nb.azn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AznApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
