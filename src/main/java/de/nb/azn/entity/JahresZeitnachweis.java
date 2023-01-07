f3a3852024989465e9d3db74c2c46dbb37d93b2e
Nikolaos Batzakakis <n.batzakakis@gmail.com>
2023-01-07 18:01:49 +0100

    JahresZeitnachweis hinzugefügt

---
+++ src/main/java/de/nb/azn/entity/JahresZeitnachweis.java
@@ -0,0 +1,28 @@
+package de.nb.azn.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "JAHRES_ZEITNACHWEIS")
+@Entity
+public class JahresZeitnachweis {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
